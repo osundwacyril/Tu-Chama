14e7fa5f24eb6c250a1dcfee9b4f5d0f2662ea4f
osundwacyril <osundwacyril@gmail.com>
2024-05-12 01:22:44 +0300

    commit

--- app/src/main/java/com/example/tu_chama/MainActivity.kt
+++ app/src/main/java/com/example/tu_chama/MainActivity.kt
@@ -19,16 +19,25 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TuChamaTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            TuChamaApp()
         }
+    }
+}
+
+@Composable
+fun TuChamaApp() {
+    TuChamaTheme {
+        TuChamaScreen()
+    }
+}
+
+@Composable
+fun TuChamaScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Greeting("Android")
     }
 }
 
@@ -43,10 +52,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 }
 
 @Preview(showBackground = true)
-
 @Composable
 fun GreetingPreview() {
-    TuChamaTheme {
-        Greeting("Cyril")
-    }
+    TuChamaApp()
 }
